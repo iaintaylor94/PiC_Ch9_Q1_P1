57a8afa8cdc9ee83c061e83e5c04fd43d3e48417
Iain Taylor <iain.taylor.94@gmail.com>
2023-08-07 14:44:17 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
+// This program uses structures to print the days date
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  struct date {
+  int day;
+  int month;
+  int year;
+  };
+
+  struct date today;
+
+  today.day = 25;
+  today.month = 9;
+  today.year = 2004;
+
+  printf ("Todays date is: %d/%d/%.2d", today.day, today.month, today.year % 100);
+  
   return 0;
 }
